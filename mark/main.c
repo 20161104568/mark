e7bc1c631c43cc67ec53e2b3405ea06564582b85
20161104568 <a20161104568@admindeiMac-9.local>
2018-06-27 08:08:45 +0800

    mark
    deafen

--- mark/main.c
+++ mark/main.c
@@ -7,9 +7,32 @@
 //
 
 #include <stdio.h>
+#define N 4
+int main(){
+    int a[N]={0};
+    int i,min,max,sum;
+    sum=0;
+    for(i=0;i<N;i++)
+    {
+        printf("请输入裁判给的分数:");
+        scanf("%d",&a[i]);
+        if(a[i]<0||a[i]>100)
+        {
+            printf("输入有误，请重新输入:");
+            scanf("%d",&a[i]);
+        }
+        
+    }
+    min=a[0];
+    max=a[0];
+    for(i=0;i<N;i++)
+    {
+        if(min>a[i])
+            min=a[i];
+        if(max<a[i])
+            max=a[i];
+        sum+=a[i];
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    }
+    printf("该选手的平均分为:%d",(sum-min-max)/(N-2));
 }
