351736b62458fabed12da63721e6ed77b28c9e9d
20161104568 <a20161104568@admindeiMac-9.local>
2018-06-27 08:44:08 +0800

    mark
    dafen

--- mark/main.c
+++ mark/main.c
@@ -6,24 +6,24 @@
 //  Copyright © 2018年 20161104568. All rights reserved.
 //
 
-#include <stdio.h>10
+#include <stdio.h>
 
 void main()
-{ float score[10];
+{ float score[5];
     float min,max,ave;
     float sum=0.0;
     int i;
-    printf("input 10 score:\n");
-    for(i=0;i<10;i++) scanf("%f",&score[i]);
+    printf("input 5 score:\n");
+    for(i=0;i<5;i++) scanf("%f",&score[i]);
     printf("\n");
     min=score[0];
     max=score[0];
-    for(i=0;i<10;i++){
+    for(i=0;i<5;i++){
         sum=sum+score[i];
         if(score[i]>max) max=score[i];
         if(score[i]<min) min=score[i];
     }
-    ave=(sum-max-min)/8;
+    ave=(sum-max-min)/3;
     printf("The max=%f\n",max);
     printf("The min=%f\n",min);
     printf("The ave=%f\n",ave);
